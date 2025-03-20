718826bd0ce121e323dea5d25ff3dd766be14272
iker.santin <iker.santin@estudiantat.upc.edu>
2025-03-20 11:07:38 +0100

    Feature/GestioActivitats: finalitzada la fusió de les dues branques de GestioActivitats i Mapa

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'map_page.dart';
 import 'calendar_page.dart';
-import 'home_page.dart';
 import 'user_page.dart';
 
 void main() {
@@ -30,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
-    HomePage(),
+    MapPage(),
     CalendarPage(),
     UserPage(),
   ];
