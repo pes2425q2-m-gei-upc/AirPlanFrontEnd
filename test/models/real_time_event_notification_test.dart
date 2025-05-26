79024bbdee982cd4e210354a2e8e98c25c32a868
iker.santin <iker.santin@estudiantat.upc.edu>
2025-05-26 12:10:22 +0200

    Feature/Notificacions: sin warnings

--- test/models/real_time_event_notification_test.dart
+++ test/models/real_time_event_notification_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:airplan/models/RealTimeEventNotification.dart';
+import 'package:airplan/models/real_time_event_notification.dart';
 
 void main() {
   group('RealTimeEventNotification', () {
